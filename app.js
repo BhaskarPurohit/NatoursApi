58db5bd0b7692f5f0bc0f77efb0b36d55731a3fc
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-17 23:55:35 +0530

    post routes

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.get("/",(req,res)=>{
     res.send("server running")
 })
 
+app.get('/first',(req,res)=>{
+    res.send("hitting the first route")
+})
+
+app.get('/sendJSON',(req,res)=>{
+    res.json({
+        response:"I am a JSON"
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("app running on port 3000")
 })
