31a73a2f998f5d4e8513d4a33ccdfa759a4e476f
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-18 22:55:48 +0530

    validate tour

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ app.get('/api/v1/tours',(req,res)=>{
     })
 })
 
+app.get('/api/v1/tours/:id',(req, res)=>{
+    console.log(req.params);
+    const id = req.params.id *1; //* converts to number
+    const tour = tours.find(el => el.id === id)
+
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message:'Invalid'
+        })
+    }
+
+    // console.log(tour);
+    res.json({
+        status:'successfully sent',
+        data:{
+            tour
+        }
+    })
+})
+
 //adding a post method
 app.post('/api/v1/tours',(req, res)=>{
     const newId = tours[tours.length-1].id + 1
