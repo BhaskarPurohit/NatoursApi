0da788b3d08a96ab902aee27165bdde51dfc0b84
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-25 01:54:15 +0530

    post req sent

--- app.js
+++ app.js
@@ -19,8 +19,43 @@ app.get('/api/v1/tours',(req,res)=>{
 })
 
 app.post('/api/v1/tours',(req, res)=>{
-    console.log(req.body)
-    res.status(200).send("Done")
+    const newId = tours[tours.length -1].id + 1
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=> {
+         res.status(201).json({  // 201 is status for Created, 200 is for OK
+            status: 'success',
+            data:{
+                tour: newTour
+            }
+         })
+        
+    })
+})
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params)
+    const id = req.params.id *1
+
+    const tour = tours.find(el => el.id === id)
+
+    // const tour = tours.find(el => el.id === req.params) //contains only the element in which the condition el.id === req.params is met
+    if(id > tours.length){
+        return res.status(404).json({
+            status: 'failed',
+            message:'id not found in the database'
+        })
+    }
+    
+        res.status(200).json({
+            status:'success',
+            data:{
+                tour
+            }
+        })
+    
 })
 
 app.listen(PORT, ()=>{
