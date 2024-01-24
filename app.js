bf9518c76d9017fdedf148e94d8d406e3a5f7c47
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-25 01:07:46 +0530

    creating a post route

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
 const PORT = 3010
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'hello from server',
-    app:"Natours"    
-})
+const fs = require('fs')
+
+app.use(express.json()) //middleware
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        results: tours.length,
+        data:{
+            tours
+        }
+    })
 })
 
-app.post('/tours',(req,res)=>{
-    res.status(200).send("post req made")
+app.post('/api/v1/tours',(req, res)=>{
+    console.log(req.body)
+    res.status(200).send("Done")
 })
 
 app.listen(PORT, ()=>{
