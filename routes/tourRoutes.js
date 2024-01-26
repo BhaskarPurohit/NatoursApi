51e0cf80fde80f04a459319147c71d5d789bab70
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-26 22:12:23 +0530

    using router params

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const fs = require('fs')
 const tourController = require('../controllers/tourController')
 
+router.param('id',(req,res, next, val)=>{
+    console.log(`tour id ${val}`);
+    next()
+})
 
 
 router
