502d5afcb3c860fc671d4a1eddae655ae57a2c84
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-29 12:57:15 +0530

    middleware for overview html route

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const userRouter = require('./routes/userRoutes')
 app.use(morgan('dev'))
 app.use(express.json()) //middleware
 
+app.use(express.static(`${__dirname}/public`))
+
 app.use((req,res,next)=>{
     console.log('middlewars')
     next()
